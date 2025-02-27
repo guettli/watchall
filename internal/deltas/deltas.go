0c3c126808a201ac20bbb82fe12b7154b76bc338
Thomas Guettler <info@thomas-guettler.de>
2025-02-27 15:18:43 +0100

    nicer output.

--- internal/deltas/deltas.go
+++ internal/deltas/deltas.go
@@ -18,7 +18,7 @@ import (
 )
 
 var resourcesToSkip = []string{
-	"events.k8s.io/Event",
+	"events.k8s.io/Event", // events do not get updated. No need to show a delta.
 }
 
 type fileType struct {
@@ -30,8 +30,8 @@ func (f fileType) String() string {
 	return filepath.Join(f.path, f.basename)
 }
 
-func Deltas(dir string, skipPatterns []string) error {
-	dir = filepath.Clean(dir)
+func Deltas(baseDir string, skipPatterns []string) error {
+	baseDir = filepath.Clean(baseDir)
 	skipRegex := make([]*regexp.Regexp, 0, len(skipPatterns))
 	for _, pattern := range skipPatterns {
 		r, err := regexp.Compile(pattern)
@@ -40,12 +40,12 @@ func Deltas(dir string, skipPatterns []string) error {
 		}
 		skipRegex = append(skipRegex, r)
 	}
-	records, err := filepath.Glob(filepath.Join(dir, "record-*"))
+	records, err := filepath.Glob(filepath.Join(baseDir, "record-*"))
 	if err != nil {
 		return fmt.Errorf("os.Glob() failed: %w", err)
 	}
 	if len(records) == 0 {
-		return fmt.Errorf("No record-YYYYMM... file found in %s", dir)
+		return fmt.Errorf("No record-YYYYMM... file found in %s", baseDir)
 	}
 	slices.Sort(records)
 	record := records[len(records)-1]
@@ -53,11 +53,11 @@ func Deltas(dir string, skipPatterns []string) error {
 
 	var files []fileType
 
-	err = filepath.WalkDir(dir, func(path string, info os.DirEntry, err error) error {
+	err = filepath.WalkDir(baseDir, func(path string, info os.DirEntry, err error) error {
 		if info.IsDir() {
 			return nil
 		}
-		if filepath.Dir(path) == dir {
+		if filepath.Dir(path) == baseDir {
 			return nil
 		}
 		if info.Name() < startTimestamp {
@@ -68,7 +68,7 @@ func Deltas(dir string, skipPatterns []string) error {
 				return nil
 			}
 		}
-		p, err := filepath.Rel(dir, filepath.Dir(path))
+		p, err := filepath.Rel(baseDir, filepath.Dir(path))
 		if err != nil {
 			return fmt.Errorf("filepath.Rel() failed: %w", err)
 		}
@@ -81,13 +81,12 @@ func Deltas(dir string, skipPatterns []string) error {
 	if err != nil {
 		return fmt.Errorf("filepath.WalkDir() failed: %w", err)
 	}
-	fmt.Printf("Found %d files for timestamp %s\n", len(files), startTimestamp)
 
 	sort.Slice(files, func(i, j int) bool {
 		return files[i].basename < files[j].basename
 	})
 	for _, file := range files {
-		err := showFile(dir, file, startTimestamp)
+		err := showFile(baseDir, file, startTimestamp)
 		if err != nil {
 			return fmt.Errorf("showFile() failed: %w", err)
 		}
@@ -95,17 +94,17 @@ func Deltas(dir string, skipPatterns []string) error {
 	return nil
 }
 
-func showFile(dir string, file fileType, startTimestamp string) error {
+func showFile(baseDir string, file fileType, startTimestamp string) error {
 	for _, resource := range resourcesToSkip {
 		if strings.HasPrefix(file.path, resource+string(filepath.Separator)) {
-			fmt.Printf("Skipping %s\n", file.String())
+			// fmt.Printf("Skipping %s\n", file.String())
 			return nil
 		}
 	}
 
 	// fmt.Printf("File: %s\n", file.String())
 
-	absDir := filepath.Join(dir, file.path)
+	absDir := filepath.Join(baseDir, file.path)
 	// find previous file
 	dirEntries, err := os.ReadDir(absDir)
 	if err != nil {
@@ -114,7 +113,6 @@ func showFile(dir string, file fileType, startTimestamp string) error {
 	sort.Slice(dirEntries, func(i, j int) bool {
 		return dirEntries[i].Name() > dirEntries[j].Name()
 	})
-
 	found := false
 	previous := ""
 	for _, entry := range dirEntries {
@@ -123,6 +121,7 @@ func showFile(dir string, file fileType, startTimestamp string) error {
 		}
 		if found {
 			previous = entry.Name()
+			break
 		}
 		if entry.Name() == file.basename {
 			found = true
@@ -132,26 +131,26 @@ func showFile(dir string, file fileType, startTimestamp string) error {
 		return fmt.Errorf("internal error. Not found: %q %s", file.path, file.basename)
 	}
 	if previous == "" {
-		fmt.Printf("No previous file found: %s\n", file.String())
+		// fmt.Printf("No previous file found: %s\n", file.String())
 		return nil
 	}
 	if previous < startTimestamp {
-		// fmt.Printf("Skipping because before %s %s\n", startTimestamp, previous)
+		// fmt.Printf("Skipping %q because before %s %s\n", file.String(), startTimestamp, previous)
 		return nil
 	}
-	compareTwoYamlFiles(filepath.Join(absDir, previous), filepath.Join(absDir, file.basename))
+	compareTwoYamlFiles(baseDir, filepath.Join(absDir, previous), filepath.Join(absDir, file.basename))
 	return nil
 }
 
-func compareTwoYamlFiles(f1, f2 string) error {
+func compareTwoYamlFiles(baseDir, f1, f2 string) error {
 	yaml1, err := os.ReadFile(f1)
 	if err != nil {
-		panic(fmt.Errorf("failed to read %q: %w", f1, err))
+		return fmt.Errorf("failed to read %q: %w", f1, err)
 	}
 
 	yaml2, err := os.ReadFile(f2)
 	if err != nil {
-		panic(fmt.Errorf("failed to read %q: %w", f2, err))
+		return fmt.Errorf("failed to read %q: %w", f2, err)
 	}
 
 	// Decode the YAML into unstructured objects
@@ -175,6 +174,7 @@ func compareTwoYamlFiles(f1, f2 string) error {
 
 	// Compare the objects
 	if equality.Semantic.DeepEqual(obj1, obj2) {
+		fmt.Printf("No changes in %q %q\n", f1, f2)
 		return nil
 	}
 	s1, err := unstructuredToString(obj1)
@@ -186,8 +186,12 @@ func compareTwoYamlFiles(f1, f2 string) error {
 		return fmt.Errorf("unstructuredToString failed %q: %w", f2, err)
 	}
 
-	diff := textdiff.Unified(f1, f2, s1, s2)
-	fmt.Printf("Diff of %q %q\n%s\n\n", f1, f2, diff)
+	diff := textdiff.Unified(filepath.Base(f1), filepath.Base(f2), s1, s2)
+	p, err := filepath.Rel(baseDir, f1)
+	if err != nil {
+		return fmt.Errorf("filepath.Rel() failed: %w", err)
+	}
+	fmt.Printf("Diff of %q %q\n%s\n\n", p, filepath.Base(f2), diff)
 	return nil
 }
 
